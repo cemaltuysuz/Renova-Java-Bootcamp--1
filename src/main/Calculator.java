9454c2c80b7cba595da652f419d2a920b39b2997
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-09-16 19:45:14 +0300

    .

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -20,10 +20,12 @@ public class Calculator {
 		// Initialize
 		input = new Scanner(System.in); 
 		result = 0L;
+		boolean loopStatus = true;
 		
-		int select = showMenu();
-		System.out.println("Seçilen :" + select);
-		
+		while(loopStatus) {
+			int select = showMenu();
+			request(select);
+		}
 	}
 	
 	/**
@@ -37,12 +39,11 @@ private static int showMenu() {
 		String [] actions = {"Toplama","Çıkarma","Çarpma","Bölme","Mod","Hafızaya Al","Hafızadakini yazdır ve temizle","Çıkış"};
 		
 		for(int i=0;i<actions.length;i++) {
-			System.out.println("["+i+"]"+" "+actions[i]);
+			System.out.println("["+(i+1)+"]"+" "+actions[i]);
 		}
-		String selected = input.nextLine();
-		int selectedInt = Integer.valueOf(selected);
+		int selected = input.nextInt();
 		
-		return selectedInt;
+		return selected;
 	}
 
 /**
@@ -55,45 +56,52 @@ private static int showMenu() {
  * result değerine eşitler ardından başka bir işlem ile devam edebilir.
  * */
 
-private static double request(int action) {
-	double localResult = 0L;
-	boolean loopStatus = true;
+private static double request(int action) {	
+	
+	double localResult = result; // Tek bir işlem için alınan her sayıda elde edilen sonucu sakladığım değişken.
+	boolean loopStatus = true; // Döngünün devamını kontrol ettiğin değişken.
 	
 	while(loopStatus) {
 		
-		System.out.println("Sayı giriniz :");
+		// Kullanıcıdan sayı istiyorum
+		
+		System.out.println("Sayı giriniz : [Sayı girişinden ayrılmak için 0]");
 		double num = input.nextDouble();
 		
-		switch(action) {
-		case 0: // Çıkış
+		if(num == 0) { 
+			// Kullanıcı sayı girisini bitiridi ise yapmak istediği işlemi seçtirmek için tekrardan menüyü gösteriyorum
+			// Böylelikle yapmış olduğu işlemlere ait sonucu global tarafa yazdırabilir ve yeni bir işlemden devam edebilir.
+			// veya sonucunu yazdırır
+			loopStatus = false;
+			int reSelect = showMenu();
+			if(reSelect == 6) {
+				result = localResult;
+			}else if(reSelect == 7) {
+				System.out.println("Sonuc :" + String.valueOf(localResult));
+			}
 			
-			break;
+			
+		}
+		
+		switch(action) {
 		case 1: 
-			sum(localResult,num);  // toplama
+			localResult = sum(localResult,num);  // toplama
 			break;
 		case 2:
-			sub(localResult,num);  // çıkarma
+			localResult = sub(localResult,num);  // çıkarma
 			break;
 		case 3:
-			multiplication(localResult,num); // çarpma
+			localResult = multiplication(localResult,num); // çarpma
 			break;
 		case 4:
-			divide(localResult,num); // bölme
+			localResult = divide(localResult,num); // bölme
 			break;
 		case 5:
-			mod(localResult,num); // mod
-			break;
-		case 6:
-			// Hafızaya alma
-			break;
-		case 7:
-			// Hafızadakini yazdır ve temizle
+			localResult = mod(localResult,num); // mod
 			break;
 		}
 		
-	}
-	
-	
+	}	
 	return localResult;
 }
 
@@ -101,30 +109,55 @@ private static double request(int action) {
 
 	public static double sum (double localResult,double num) {
 		double result = localResult + num;
+		System.out.println(" -> " + result);
 		return result;
 	}
 	// Çıkarma işlemi
 	
 	public static double sub (double localResult,double num) {
-		double result = localResult - num;
+		double result;
+		if(localResult == 0) {
+			result = num;
+		}else {
+			result = localResult - num;
+		}
+		System.out.println(" -> " + result);
 		return result;
 	}
 	// Bölme işlemi
 	
 	public static double divide (double localResult,double num) {
-		double result = localResult / num;
+		double result;
+		if(localResult == 0) {
+			result = num;
+		}else {
+			result = localResult / num;
+		}
+		System.out.println(" -> " + result);
 		return result;
 	}
 	// Çarpma işlemi
 	
 	public static double multiplication (double localResult,double num) {
-		double result = localResult * num;
+		double result;
+		if(localResult == 0) {
+			result = num;
+		}else {
+			result = localResult * num;
+		}
+		System.out.println(" -> " + result);
 		return result;
 	}
 	// Mod Alma işlemi
 	
 	public static double mod (double localResult,double num) {
-		double result = localResult % num;
+		double result;
+		if(localResult == 0) {
+			result = num;
+		}else {
+			result = localResult % num;
+		}
+		System.out.println(" -> " + result);
 		return result;
 	}
 
---
+++ src/main/deneme.java
@@ -0,0 +1,21 @@
+package main;
+
+public class deneme {
+	
+	static double result ;
+	
+	public static void main (String [] args) {
+		
+		int secilen;
+		
+		// menü göster (toplama)
+		
+		// for , while
+		
+		swi
+		
+		
+		
+	}
+	
+}
