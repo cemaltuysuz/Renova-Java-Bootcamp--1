e3d62d38c8b0fa4ab04f70b5682d2806e8392986
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-09-16 17:09:42 +0300

    showMenu() method

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,10 +1,46 @@
 package main;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Cemal Tüysüz
+ * @date 16.09.2021
+ * 
+ * */
+
 
 public class Calculator {
 	
+	// Declare
+	static Scanner input; 
+	
 	public static void main(String [] args) {
 		
+		// Initialize
+		input = new Scanner(System.in); 
+		
+		int select = showMenu();
+		System.out.println("Seçilen :" + select);
 		
 	}
-
+	
+	/**
+	 * Bu methodum ile kullanıcıya işlemlerin olduğu bir menü gösteriyor 
+	 * ve bu işlemlerden bir seçim yapmasını istiyorum. Kullanıcı seçimini yaptığı 
+	 * zaman bu değeri geri döndürüyorum.
+	 * */
+	
+private static int showMenu() {
+		
+		String [] actions = {"Toplama","Çıkarma","Çarpma","Bölme","Mod","Hafızaya Al","Hafızadakini yazdır ve temizle","Çıkış"};
+		
+		for(int i=0;i<actions.length;i++) {
+			System.out.println("["+i+"]"+" "+actions[i]);
+		}
+		String selected = input.nextLine();
+		int selectedInt = Integer.valueOf(selected);
+		
+		return selectedInt;
+	}
+	
 }
